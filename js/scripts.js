2e34be20d18fb0275e78f323e137a9fb86cb907d
Ptashkina Liliya <efekta9@gmail.com>
2019-03-04 10:51:36 +0300

    no message

--- js/scripts.js
+++ js/scripts.js
@@ -1591,4 +1591,67 @@ $(window).on('load', function() {
         myMap.geoObjects.add(myPlacemark);
         myMap.behaviors.disable(['scrollZoom']);
     });
+
+        // init cubeportfolio
+        $('#gallery-items').cubeportfolio({
+            filters: '#gallery-filter1, #gallery-filter2',
+            loadMore: '#js-loadMore-masonry-projects',
+            loadMoreAction: 'click',
+            layoutMode: 'grid',
+            defaultFilter: '*',
+            animationType: 'quicksand',
+            gapHorizontal: 0,
+            gapVertical: 0,
+            gridAdjustment: 'responsive',
+            mediaQueries: [{
+                width: 1500,
+                cols: 5,
+            }, {
+                width: 1100,
+                cols: 5,
+            }, {
+                width: 800,
+                cols: 2
+            }, {
+                width: 480,
+                cols: 1,
+                options: {
+                    caption: '',
+                    gapHorizontal: 25,
+                    gapVertical: 10,
+                }
+            }],
+            caption: 'zoom',
+            displayType: 'fadeIn',
+            displayTypeSpeed: 100,
+
+            // lightbox
+            lightboxDelegate: '.cbp-lightbox',
+            lightboxGallery: true,
+            lightboxTitleSrc: 'data-title',
+            lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+
+            // singlePage popup
+            singlePageDelegate: '.cbp-singlePage',
+            singlePageDeeplinking: true,
+            singlePageStickyNavigation: true,
+            singlePageCounter: '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+            singlePageCallback: function(url, element) {
+                // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+                var t = this;
+
+                $.ajax({
+                        url: url,
+                        type: 'GET',
+                        dataType: 'html',
+                        timeout: 30000
+                    })
+                    .done(function(result) {
+                        t.updateSinglePage(result);
+                    })
+                    .fail(function() {
+                        t.updateSinglePage('AJAX Error! Please refresh the page!');
+                    });
+            },
+        });
 });
